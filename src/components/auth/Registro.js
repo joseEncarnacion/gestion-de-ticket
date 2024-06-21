80231f84acbd223bbfebfc9a097ca2f00e5044da
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2024-06-21 10:07:50 -0400

    configuraciones ruteo dinamico a las vistas login y registro

---
+++ src/components/auth/Registro.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Registro = () => {
+    return ( 
+        <div>
+
+            Aqui deb estar la ruta registro 
+
+            <h1 className='App-header'> registro</h1> 
+    
+        </div>
+     );
+}
+ 
+export default Registro;
