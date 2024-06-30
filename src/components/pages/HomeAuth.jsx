7c468d5f7db0deac603e94a8fde0069d305dd5dd
Jose Luis Encarnacion <jose.lencarnacion1@gmail.com>
2024-06-30 14:38:05 -0400

    Creacion del componente home para usuarios autenticados

---
+++ src/components/pages/HomeAuth.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const HomeInitialAUTH = () => {
+    return ( 
+        <div className='text-center '>
+            <h1>Home de inicio para usuarios autenticados</h1>
+        </div>
+     );
+}
+ 
+export default HomeInitialAUTH;
